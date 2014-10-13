398fc73e27ddad79732a4aad2b385a6b5b129be0
Patrick Love <plove13x@gmail.com>
2014-10-13 19:23:56 -0400

    Legitimizes avatar functionality

--- app/js/controllers/message_controller.js
+++ app/js/controllers/message_controller.js
@@ -1,15 +1,26 @@
 Crack.MessageController = Ember.ObjectController.extend({
 	test: 1,
 
-	hash: function() {
-		// console.log('Hay');
+	// hash: function() {
+	// 	// console.log('Hay');
+	// 	var hash = md5(this.get('model.email'));
+	// 	// console.log(hash);
+	// 	this.model.set('avatarUrl', 'http://www.gravatar.com/avatar/' + hash + '?s=60&d=http://placekitten.com/g/40/50');
+	// 	// console.log(this.model); 
+	// 	// return '';
+	// 	// return 'http://www.gravatar.com/avatar/' + hash + '?s=60&d=http://placekitten.com/g/40/50';
+	// 	}.property('model.avatarUrl'),
+
+	avatarUrl: function() {
 		var hash = md5(this.get('model.email'));
-		// console.log(hash);
-		this.model.set('avatarUrl', 'http://www.gravatar.com/avatar/' + hash + '?s=60&d=http://placekitten.com/g/40/50');
-		// console.log(this.model); 
-		// return '';
-		// return 'http://www.gravatar.com/avatar/' + hash + '?s=60&d=http://placekitten.com/g/40/50';
-		}.property('model.avatarUrl'),
+		return 'http://www.gravatar.com/avatar/' + hash + '?s=60&d=http://placekitten.com/g/40/50';
+	}.property('model.email')
+
+	// Watches model.email for changes that will cause this computed property to recompute...If model.email was instead an array called favoriteFoods, write 
+	// 'favoriteFoods.@each' to monitor items being added or deleted from array since the value of property (the actual array itself) wouldn't be changing... 
+
+
+
 
 	// console: function() {
 	// 	console.log('hay');
@@ -18,7 +29,17 @@ Crack.MessageController = Ember.ObjectController.extend({
 });
 
 
+
+
+
+
+
+
 	// hash: function() {
 	// 	var hash = md5(this.get('model.email'));
 	// 	return hash;
 	// }.property('model.hash')
+
+
+
+
